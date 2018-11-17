d526e22f585112e33afa6e5813ae95ffd23ab3d0
Jbrown95 <browningjustin95@gmail.com>
2018-11-17 09:08:10 -0500

    created js file for dashboard and created js functions to slide to next page onclick

---
+++ public/js/dashboard.js
@@ -0,0 +1,9 @@
+function workSlide() {
+    var b = document.getElementById('work-content').offsetTop;
+    window.scroll({top: b, left: 0, behavior: 'smooth'});
+}
+
+function slideBtn() {
+    var b = document.getElementById('work-content').offsetTop;
+    window.scroll({top: b, left: 0, behavior: 'smooth'});
+}
