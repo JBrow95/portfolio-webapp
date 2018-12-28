d2358d136a807cf793d1793389f1a14d45cd1cbc
Jbrow95 <browningjustin95@gmail.com>
2018-12-27 22:25:49 -0500

    created new slide btn function and added animation onclick

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,11 +1,25 @@
 function skillSlide() {
     var b = document.getElementById('skills-content').offsetTop;
     window.scroll({top: b, left: 0, behavior: 'smooth'});
+
+    var circle = document.getElementById('trans');
+    var dots = circle.querySelectorAll('button')
+
+    dots.forEach(element => {
+        element.style.borderColor = "#EDF5E1";
+    });
 }
 
 function slideupBtn() {
     var n = document.getElementById('body');
     window.scroll({top: n, left: 0, behavior: 'smooth'});
+
+    var circle = document.getElementById('trans');
+    var dots = circle.querySelectorAll('button')
+
+    dots.forEach(element => {
+        element.style.borderColor = "#EDF5E1";
+    });
 };
 
 function bgChange() {
@@ -13,3 +27,15 @@ function bgChange() {
     x.style.transition = "all 1s ease";
 }
 
+function slide3Btn() {
+    var b = document.getElementById('about').offsetTop;
+    window.scroll({top: b, left: 0, behavior: 'smooth'});
+
+    var circle = document.getElementById('trans');
+    var dots = circle.querySelectorAll('button')
+
+    dots.forEach(element => {
+        element.style.borderColor = "#05386B";
+        element.style.transition = "1s"
+    });
+}
